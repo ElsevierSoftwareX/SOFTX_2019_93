d77bd09ee2cec0361be7a04cc2ee874dd6bd0510
Ben Whale <bwhale@maths.otago.ac.nz>
2011-07-07 10:05:49 +1200

    Fixed minor bug in convergence rate table layout for hdfvis.py.
    The bug ment that, for the first row of data, if a number was not greater than 10 then a space would be missing between entries. This would throw off the alignment of the columns.

--- Code/HDF_Code/DataAnalysis/simulate.py
+++ Code/HDF_Code/DataAnalysis/simulate.py
@@ -16,7 +16,7 @@ parser.add_argument('-terr','-times-error',nargs='+',help=\
 parser.add_argument('-tani','-times-start-animation',nargs='+',help=\
 """A list of time ranges in the format "[t0,t1]" to be used for the start and stop times of animations.""")
 parser.add_argument('-tplo','-times-plot',nargs='+', help=\
-"""A list of all times for which plots of the specified data types should be made.""")
+"""A list of all times for which plots of the specified data types should be made. If this is not specified it defaults to the value of -terr""")
 
 # Parse data types
 parser.add_argument('-dg','-dgTypes',nargs='+',help=\
@@ -29,7 +29,11 @@ if args.dg is None:
 
 if args.tani is not None:
     args.tani = [eval(tani) for tani in args.tani]
+    
+if args.tplo is None:
+    args.tplo = args.terr
 
+# Do conversions for commands that can take multiple times and dgs.
 def bash_string(sarray,prefix):
     rs = ''
     for s in sarray:
@@ -42,44 +46,41 @@ args.terr_bash = bash_string(args.terr,'-t')
 # Run main.py. Need to do this to get file name if it wasn't specified
 main_run = "python -O ../Computation/main.py"
 print "MAIN CALCULATION: "+main_run
-#args.mfile =  subprocess.Popen(main_run,\
-#    shell=True,stdout=subprocess.PIPE).communicate()[0]
+args.mfile =  subprocess.Popen(main_run,\
+    shell=True,stdout=subprocess.PIPE).communicate()[0]
 
 if args.f is None:
     args.f = args.mfile
 
 errorNum_run = []
-errorNum_run += ["python ./errorNumerical %s %s %s"%\
+errorNum_run += ["python ./errorNumerical.py %s %s %s"%\
         (args.terr_bash,args.dg,args.f)]
-#for time in args.terr:
-#    errorNum_run += ["python ./errorNumerical -t %s -dg %s %s"%\
-#        (time,args.dg,args.f)]
         
 hdfvis_error = []
 for time in args.terr:
-    hdfvis_error += ['python ./hdfvis %s err -t %s %s'%\
+    hdfvis_error += ['python ./hdfvis.py %s err -t %s %s'%\
         (args.dg,time,args.f)]
 
 hdfvis_plot = []
 for time in args.tplo:
-    hdfvis_plot  += ['python ./hdfvis %s plot -t %s %s'%\
+    hdfvis_plot  += ['python ./hdfvis.py %s plot -t %s %s'%\
         (args.dg,time,args.f)]
         
 hdfvis_ani = []
 for tani in args.tani:
-        hdfvis_ani  += ['python ./hdfvis %s ani -t0 %s -t1 %s %s'%\
+        hdfvis_ani  += ['python ./hdfvis.py %s ani -t0 %s -t1 %s %s'%\
             (args.dg,tani[0],tani[1],args.f)]
 
 
 for s in errorNum_run:
     print "ERROR CALCULATION: "+s
-#    subprocess.call(s,shell=True)
+    subprocess.call(s,shell=True)
 for s in hdfvis_error: 
     print "ERROR VISULISATION: "+s    
-#    subprocess.call(s,shell=True)
+    subprocess.call(s,shell=True)
 for s in hdfvis_plot: 
     print "GRAPHING: "+s    
-#    subprocess.call(s,shell=True)
+    subprocess.call(s,shell=True)
 for s in hdfvis_ani: 
     print "ANIMATION: "+s    
-#    subprocess.call(s,shell=True)
+    subprocess.call(s,shell=True)
