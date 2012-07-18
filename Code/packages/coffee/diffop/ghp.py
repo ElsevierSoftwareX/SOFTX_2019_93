Fixed off by one bug.
Spinsfast py takes a variable lmax. This is the maxium lmax that is used in computation, not the l value that bounds all other variables. The consequence is that if you are iterating over all l values you must use range(lmax + 1). This has caused issues in the transformations, salm, and ghp.py

Code/packages/coffee/diffop/ghp.py
@@ -29,15 +29,15 @@ class eth(object):
             lmax
             )
         #print "salm = %s"%salm
-        for j in range(lmax):
+        for j in range(lmax + 1):
             sm_values = salm[:,j]
             eigenvalues = np.vectorize(_eth_eigen)(spins, j)
-            eigenvalues = np.lib.stride_tricks.as_strided(
-                eigenvalues,
-                sm_values.shape,
-                (eigenvalues.itemsize, 0)
-                )
-            r_salm[:,j] = eigenvalues * sm_values
+#            eigenvalues = np.lib.stride_tricks.as_strided(
+#                eigenvalues,
+#                sm_values.shape,
+#                (eigenvalues.itemsize, 0)
+#                )
+            r_salm[:,j] = eigenvalues[:, np.newaxis] * sm_values
         #print "r_salm = %s"%r_salm
         if Ntheta is not None:
             r_u = sfpy.backward(r_salm, Ntheta, Nphi)
@@ -81,15 +81,16 @@ class ethp(object):
             lmax = u.lmax
         r_salm = sfpy.sfpy_salm(np.empty_like(salm, dtype=np.typeDict['complex']), spins - 1, lmax)
         #print "salm = %s"%salm
-        for j in range(lmax):
+        for j in range(lmax + 1):
             sm_values = salm[:,j]
             eigenvalues = np.vectorize(_ethp_eigen)(spins, j)
-            eigenvalues = np.lib.stride_tricks.as_strided(
-                eigenvalues,
-                sm_values.shape,
-                (eigenvalues.itemsize, 0)
-                )
-            r_salm[:,j] = eigenvalues * sm_values
+#            eigenvalues = np.lib.stride_tricks.as_strided(
+#                eigenvalues,
+#                sm_values.shape,
+#                (eigenvalues.itemsize, 0)
+#                )
+#            r_salm[:,j] = eigenvalues * sm_values
+            r_salm[:,j] = eigenvalues[:, np.newaxis] * sm_values
         #print "r_salm = %s"%r_salm
         if Ntheta is not None:
             r_u = sfpy.backward(r_salm, Ntheta, Nphi)

Code/packages/coffee/swsh/spinsfastpy/backward_transform.py
@@ -114,7 +114,7 @@ def _backward_Gmm(Ntheta, Nphi, lmax, Nmaps, Gmm):
          section 2.3
     """
     f = np.empty((Nmaps,Ntheta*Nphi), dtype = typeDict['complex'])
-    Nm = 2 * lmax +1
+    Nm = 2 * lmax + 1
     NGmm = Nm * Nm
     for i in range(Nmaps):
         sf.spinsfast_backward_transform(f[i], Ntheta, Nphi, lmax, Gmm[i*NGmm:(i+1)*NGmm])

Code/packages/coffee/swsh/spinsfastpy/salm.py
@@ -92,7 +92,7 @@ class sfpy_salm(np.ndarray):
     def __str__(self):
         s = "spins = %s,\n"
         for spin in self.spins:
-            for l in range(self.lmax):
+            for l in range(self.lmax + 1):
                 s +="(%f, %f): %s\n"%(spin, l, repr(self[spin,l].view(np.ndarray)))
         return s 
        
@@ -140,13 +140,13 @@ class sfpy_salm(np.ndarray):
         if len(key) == 1:
             r_key = spin_key,
         elif len(key) == 2:
-            if key[1] >= self.lmax or key[1] < 0:
+            if key[1] > self.lmax or key[1] < 0:
                 raise IndexError("order out of bounds")
             l_ind = _lm_ind(key[1],-key[1])
             order_key = slice(l_ind, l_ind + 2*key[1] +1)
             r_key = spin_key, order_key
         elif len(key) == 3:
-            if abs(key[2]) >= self.lmax:
+            if abs(key[2]) > self.lmax:
                 raise IndexError("degree out of bounds")
             order_key = _lm_ind(key[1],key[2])
             r_key = spin_key, order_key