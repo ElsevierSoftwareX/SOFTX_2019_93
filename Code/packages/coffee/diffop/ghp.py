b783c39ed2388917d950bad731f2abdcb37fb312
Ben Whale <bwhale@maths.otago.ac.nz>
2013-01-18 15:32:31 +1300

    Rationalisation of code for ghp operators. Operators also extended to act on sralm objects.

--- Code/packages/coffee/diffop/ghp.py
+++ Code/packages/coffee/diffop/ghp.py
@@ -5,6 +5,11 @@ import numpy as np
 
 from coffee.swsh import spinsfastpy as sfpy
 
+#This module could be reimplemented using meta-classes as eth and ethp
+#essentially just differ by a boolean value. I have already tried to avoid
+#having copies of essentially the same code, but this would completely
+#remove this remainin duplicates
+
 class eth(object):
     
     def __init__(self):
@@ -14,40 +19,76 @@ class eth(object):
         """
 
     def __call__(self, u, spins = None, lmax = None):
-        Ntheta = None
-        if spins is not None and lmax is not None:
-            Ntheta, Nphi = u.shape
-            salm = sfpy.forward(u, spins, lmax)
-            spins = np.asarray(spins)
+        if isinstance(u, sfpy.salm.sfpy_sralm):
+            return _eval_sralm(self, u)
         else:
-            salm = u
-            spins = u.spins
-            lmax = u.lmax
-        r_salm = sfpy.sfpy_salm(
-            np.empty_like(np.asarray(salm), dtype=np.typeDict['complex']), 
+            return self._eval_salm(u, spins, lmax)
+
+    def _eval_salm(self, u, spins = None, lmax = None):
+        Ntheta = None
+        Nphi = None
+        salm, spins, lmax = _transform_to_harmonic_space(
+            u, spins, lmax, Ntheta, Nphi
+            )
+        r_salm = sfpy.salm.sfpy_salm(
+            np.empty_like(salm, dtype=np.typeDict['complex']), 
             spins + 1, 
             lmax
             )
-        #print "salm = %s"%salm
-        for j in range(lmax + 1):
-            sm_values = salm[:,j]
-            eigenvalues = np.vectorize(_eth_eigen)(spins, j)
-#            eigenvalues = np.lib.stride_tricks.as_strided(
-#                eigenvalues,
-#                sm_values.shape,
-#                (eigenvalues.itemsize, 0)
-#                )
-            r_salm[:,j] = eigenvalues[:, np.newaxis] * sm_values
-        #print "r_salm = %s"%r_salm
-        if Ntheta is not None:
-            r_u = sfpy.backward(r_salm, Ntheta, Nphi)
-            return r_u
-        else:
-            return r_salm
-        
+        _do_derivative(salm, spins, lmax, _eth_eigen, r_salm)
+        return _transform_from_harmonic_space(r_salm, Ntheta, Nphi)
   
     def __repr__(self):
         return "eth"
+
+
+class ethp(object):
+    
+    def __init__(self):
+        """
+        Implements the differential operator given by
+        \eth'{}_sY_{lm} = -\sqrt{l(l+1)-s(s-1)}{}_{s-1}Y_{lm}
+        """
+
+    def __call__(self, u, spins = None, lmax = None):
+        if isinstance(u, sfpy.salm.sfpy_sralm):
+            return _eval_sralm(self, u)
+        else:
+            return self._eval_salm(u, spins, lmax)
+
+    def _eval_salm(self, u, spins = None, lmax = None):
+        Ntheta = None
+        Nphi = None
+        salm, spins, lmax = _transform_to_harmonic_space(
+            u, spins, lmax, Ntheta, Nphi
+            )
+        r_salm = sfpy.salm.sfpy_salm(
+            np.empty_like(salm, dtype=np.typeDict['complex']), 
+            spins - 1, 
+            lmax
+            )
+        _do_derivative(salm, spins, lmax, _ethp_eigen, r_salm)
+        return _transform_from_harmonic_space(r_salm, Ntheta, Nphi)
+  
+    def __repr__(self):
+        return "ethp"
+
+def _eval_sralm(self, u):
+    rv_temp = self._eval_salm(u[:,0,:])
+    rv = np.empty(
+        (rv_temp.shape[0],) + (u.shape[1],) + (rv_temp.shape[1],),
+        dtype = u.dtype
+        )
+    rv[:,0,:] = np.asarray(rv_temp)
+    for i in range(1,u.shape[1]):
+        rv[:, i, :] = self._eval_salm(u[:, i, :])
+    return sfpy.salm.sfpy_sralm(
+        rv,
+        u.spins,
+        u.lmax,
+        cg=u.cg,
+        bandlimit_multiplication=u.bl_mult
+        )
 
 def _eth_eigen(s, j):
     if abs(s+1) > j:
@@ -61,42 +102,24 @@ def _ethp_eigen(s, j):
     else:
         return -math.sqrt(j*(j+1)-s*(s-1))
 
-class ethp(object):
-    
-    def __init__(self):
-        """
-        Implements the differential operator given by
-        \eth'{}_sY_{lm} = -\sqrt{l(l+1)-s(s-1)}{}_{s-1}Y_{lm}
-        """
+def _transform_to_harmonic_space(u, spins, lmax, Nthetea, Nphi):
+    if spins is not None and lmax is not None:
+        Ntheta, Nphi = u.shape
+        salm = sfpy.forward(u, spins, lmax)
+        spins = np.asarray(spins)
+    else:
+        salm = u
+        spins = u.spins
+        lmax = u.lmax
+    return salm, spins, lmax
 
-    def __call__(self, u, spins = None, lmax = None):
-        Ntheta = None
-        if spins is not None and lmax is not None:
-            Ntheta, Nphi = u.shape
-            salm = sfpy.forward(u, spins, lmax)
-            spins = np.asarray(spins)
-        else:
-            salm = u
-            spins = u.spins
-            lmax = u.lmax
-        r_salm = sfpy.sfpy_salm(np.empty_like(salm, dtype=np.typeDict['complex']), spins - 1, lmax)
-        #print "salm = %s"%salm
-        for j in range(lmax + 1):
-            sm_values = salm[:,j]
-            eigenvalues = np.vectorize(_ethp_eigen)(spins, j)
-#            eigenvalues = np.lib.stride_tricks.as_strided(
-#                eigenvalues,
-#                sm_values.shape,
-#                (eigenvalues.itemsize, 0)
-#                )
-#            r_salm[:,j] = eigenvalues * sm_values
-            r_salm[:,j] = eigenvalues[:, np.newaxis] * sm_values
-        #print "r_salm = %s"%r_salm
-        if Ntheta is not None:
-            r_u = sfpy.backward(r_salm, Ntheta, Nphi)
-            return r_u
-        else:
-            return r_salm
-  
-    def __repr__(self):
-        return "ethp"
+def _do_derivative(salm, spins, lmax, eigen_calc, r_salm):
+    for j in range(lmax + 1):
+        sm_values = salm[:,j]
+        eigenvalues = np.vectorize(eigen_calc)(spins, j)
+        r_salm[:,j] = eigenvalues[:, np.newaxis] * sm_values
+
+def _transform_from_harmonic_space(r_salm, Ntheta, Nphi):
+    if Ntheta is not None:
+        return sfpy.backward(r_salm, Ntheta, Nphi)
+    return r_salm
